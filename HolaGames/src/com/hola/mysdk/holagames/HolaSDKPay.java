4801cbc9876d7a474b44a24404daa3cb63beb306
wlcaption <wlcaption@qq.com>
2016-12-12 13:53:45 +0800

    pay

--- HolaGames/src/com/hola/mysdk/holagames/HolaSDKPay.java
+++ HolaGames/src/com/hola/mysdk/holagames/HolaSDKPay.java
@@ -1,7 +1,12 @@
 package com.hola.mysdk.holagames;
 
+import com.hola.alipay.sdk.AlipayActivity;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class HolaSDKPay extends Activity {
 	@Override
@@ -12,6 +17,27 @@ public class HolaSDKPay extends Activity {
 	}
 	
 	private void initView(){
+		View view_back = findViewById(R.id.pay_backIcon); //返回上层
+		view_back.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setClass(HolaSDKPay.this, AlipayActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		View view_next_step = findViewById(R.id.next_step);
+		view_next_step.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setClass(HolaSDKPay.this, AlipayActivity.class);
+				startActivity(intent);
+			}
+		});
 		
 	}
 
